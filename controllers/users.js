5f5c5b970b7cce0c28b152d5cbadf7c0655019b5
georgiiv <georgi.iv.ivanov@abv.bg>
2020-09-02 19:03:36 +0300

    Added login

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require("passport");
 const db = require("../models/");
 
 router.get('/', async (req, res) => {
@@ -9,17 +10,30 @@ router.post('/', async (req, res) => {
 	const {username, email, password, repeatPassword} = req.body;
 
 	if(await db.User.findByUsername(username)){
-		console.log("wtffffffffff")
 		res.send("Username already exists");
 	}
 	else if(await db.User.findByEmail(email)){
-		console.log("wtaaaaaaaaaa")
 		res.send("Email already exists");
 	}else{
-		console.log("zzzzzzzzzzzzzzz")
 		await db.User.createUser(username, email, password, repeatPassword);
 		res.send("User created");
 	}
+});
+
+router.post('/login', async (req, res) => {
+	passport.authenticate("local", function(err, user, info){
+		if (err) { return next(err); } 
+        // user will be set to false, if not authenticated
+        if (!user) {
+			//info contains the error message
+            res.status(401).json(info);
+        } else {
+            // if user authenticated maintain the session
+            req.logIn(user, function() {
+                res.send("Authenticated")
+            })
+        } 
+	})(req, res);
 });
 
 module.exports = router;
--- passport.js
+++ passport.js
@@ -2,26 +2,35 @@ const LocalStrategy = require("passport-local").Strategy;
 const db = require("./models");
 const bcrypt = require("bcrypt");
 
-function initialise(passport){
+function initialise(passport) {
 	console.log("Passport initialised")
 
 	const authenticateUser = async (username, password, done) => {
 		let User;
-		if(User = await db.User.findOne({where: {username: username}})){
-			if(User.password == password){
-				return done(null, User);
-			}
+		if (User = await db.User.findOne({ where: { userName: username } })) {
+			bcrypt.compare(password, User.password, (err, isMatch) => {
+				if (err) {
+					console.log(err);
+				}
+				if (isMatch) {
+					return done(null, User);
+				} else {
+					return done(null, false, { message: "Password is incorrect" });
+				}
+			});
+		} else {
+			return done(null, false, { message: "No user with that username" });
 		}
 	}
 
-	passport.use(new LocalStrategy({usernameField: "username", passwordField: "password"}, authenticateUser))
+	passport.use(new LocalStrategy({ usernameField: "username", passwordField: "password" }, authenticateUser))
 
 	passport.serializeUser((user, done) => {
 		done(null, user.id);
 	});
 
 	passport.deserializeUser(async (id, done) => {
-		await db.User.findOne({where: {id: id}});
+		await db.User.findOne({ where: { id: id } });
 		return done(null, db.User);
 	})
 }
