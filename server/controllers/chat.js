6eb6badd2288536a1f797ebf62e703ba27eb4ac1
georgiiv <georgi.iv.ivanov@abv.bg>
2020-09-06 19:09:17 +0300

    chat controller testing route

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -6,6 +6,19 @@ router.get('/', async (req, res) => {
 	res.send(req.user);
 });
 
+router.post('/:username', async (req, res) => {
+	let message = req.body.message;
+
+	let reciever = await db.User.findByUsername(req.params.username);
+	if (reciever){
+		await db.Chat.createMessage(req.user.id, reciever.id, message);
+		res.send("Message inserted");
+	}else{
+		res.send(404, "Reciever not found")
+	}
+});
+
+//testing purposes only
 router.get('/:username', async (req, res) => {
 	let message = req.query.message;
 
@@ -14,7 +27,7 @@ router.get('/:username', async (req, res) => {
 		await db.Chat.createMessage(req.user.id, reciever.id, message);
 		res.send("Message inserted");
 	}else{
-		res.send(404, "User not found")
+		res.send(404, "Reciever not found")
 	}
 });
 
